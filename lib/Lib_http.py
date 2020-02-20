7ba8f9a66686492aeeb95c3926dd1f1b8395af2a
nemo-xs <nemox00@mail.ru>
2020-02-20 05:10:02 +0300

    test commit VsCode

--- lib/Lib_http.py
+++ lib/Lib_http.py
@@ -1,7 +1,37 @@
 import requests
 
+
+# region #* HTTP запросы, библиотека requests ---------------------------------
+# ** https://stepik.org/lesson/24471/step/1?unit=6780
+"""
+в http протоколе объявлено несколько методов
+#** get - запрос html кода, картинок и тп. не изменяет данные на сервере.
+возвращает response object, где response это класс, описанный внутри requests
+в качестве атрибутов там есть статус коды, хедеры, и само содержимое (см ниже)
+#? res = requests.get('http://rabbitroom.ru/')  # сам запрос
+#? print(res.status_code)  # код ответа (200, 404 и.т.д.)
+#? print(res.url)  # вывод url страницы
+#? print(res.headers["Content-Type"])  # словарь headers, значение Content-Type
+#? print(res.content)  # содержимое ответа в бинарном виде
+#? print(res.text)  # содердимое ответа в виде такста
+#** post - предназначен для изменения данных на сервере. Смена паролей,
+банковские транзакции и другая инфо, которая пОстится
+#** res - response - ответ на запрос от сервера. Сначала код ответа (200, 404,
+503) затем служебная информация, пустая строка и сам ответ.
+"""
+# #* format stroke на примере requests
+# template = "Response from {0.url} whith code {0.status_code}"
+# res = requests.get('http://rabbitroom.ru/')
+# print(template.format(res))
+""" #* Пример записи в файл полученного ответа (картинка)
+whith open("file.png", "wb") as f:
+    f.write(res.content)
+"""
+# endregion -------------------------------------------------------------------
+
+
 url = 'http://example.com'
-par = {'key1':'value1', 'key2': 'value2'}
+par = {'key1': 'value1', 'key2': 'value2'}
 cookies = {'cookies_are': 'working'}
 
 r = requests.get(url, params=par, cookies=cookies)
